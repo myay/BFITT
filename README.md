3269aee8f52e9f0b76c50a4e04a8041934244ecf
Mikail <mikail.yayla@tu-dortmund.de>
2020-06-07 01:31:14 +0200

    Work on more test cases

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # BFITT: Bit Flip Injection Into PyTorch Tensors
-This repo provides efficient CUDA kernel based Bit Flip Injection into PyTorch Tensors for all PyTorch for any number of dimensions and all data types except torch.bool and torch.float16.
+
+---
+This repository provides efficient CUDA kernel-based bit flip injection into PyTorch tensors for any number of dimensions and all data types except torch.bool and torch.float16.
 
 Using BFITT:
-- ```cd src/``` and install with ```python setup.py install``` in folder src
+- install with ```python setup.py install --user```
 - ```import torch``` and ```import bfitt```
-- use ```bfitt.bfi_8bit(input, p01, p10)``` to call the bfi function that injects bit flips into all 8 bit data types, where ```p01``` is the probability in decimal for flipping 0->1 and ```p10``` for 1->0
-- in interactive mode, try ```bfitt.bfi_8bit(torch.ones(2,2,2,2).type(torch.uint8).cuda(), 0.03, 0)```, to test and see the effect of bit flips
+- in interactive mode, try ```bfitt.bfi_8bit(torch.ones(2,2,2,2).type(torch.uint8).cuda(), 0.03, 0)```, to test the installation and see the effect of bit flips
+- we use ```bfitt.bfi_8bit(input, p01, p10)``` to call the bfi function that injects bit flips into all 8 bit data types, where ```input``` is a PyTorch tensor and ```p01``` is the probability in decimal for flipping 0->1 and ```p10``` for 1->0
 
-There are functions available for 8, 16, 32, and 64 bits:
-- bfi_8bit
-- bfi_16bit
-- bfi_32bit
-- bfi_64bit
+There are functions available for datatypes with 8, 16, 32, and 64 bits:
+- bfitt.bfi_8bit(input, p01, p10)
+- bfitt.bfi_16bit(input, p01, p10)
+- bfitt.bfi_32bit(input, p01, p10)
+- bfitt.bfi_64bit(input, p01, p10)
 
-For configuring threads per block before compilation, see ```src/bfitt_kernels.cu``` and set the macros ```TPB_X```, ```TPB_Y```, and ```TPB_Z```.
+For configuring threads per block before compilation, see ```bfitt/bfitt_kernels.cu``` and set the macros ```TPB_X```, ```TPB_Y```, and ```TPB_Z```.
 
 ---
 This software contains source code provided by NVIDIA Corporation.
--- tests/bfitt_xbit_tests.py
+++ tests/bfitt_xbit_tests.py
@@ -16,76 +16,79 @@ def cuda_profiler(profile_function, input, p01, p10):
     output = profile_function(input, p01, p10)
     end.record()
     torch.cuda.synchronize()
-    print(start.elapsed_time(end))
+    print("Run time (ms):", start.elapsed_time(end))
     return output
 
 def main():
-    p01 = 0.01
-    p10 = 0
-    test_range = 7
-    dim_cases = [[2], [2, 2], [2, 2, 2], [2 ,2, 2, 2]]
-    rest_cases = [[1,1,2,2], [2,2,1,1], [4,2,2,2], [2,4,2,2], [2,2,4,2], [2,2,2,2], [1,1,1,1,1]]
-    for rest_case in rest_cases:
-        dim_cases.append(rest_case)
+    reps = 1
+    bfprobs = [[0.03, 0], [0, 0.03], [0, 0]]
+    test_range = 2
+    dim_cases = [[2], [2, 2], [2, 2, 2], [2 ,2, 2, 2], [2 ,2, 2, 2, 2]]
+    # rest_cases = [[1,1,2,2], [2,2,1,1], [4,2,2,2], [2,4,2,2], [2,2,4,2], [2,2,2,2], [1,1,1,1,1]]
+    # for rest_case in rest_cases:
+    #     dim_cases.append(rest_case)
 
-    for dim_case in dim_cases:
-        for batch_mult in range(1,test_range):
-            test_dims = [d*(pow(2,batch_mult-1)) for d in dim_case]
+    for rep in range(reps):
+        for p_case in bfprobs:
+            for dim_case in dim_cases:
+                for batch_mult in range(1,test_range):
+                    test_dims = [d*(pow(2,batch_mult-1)) for d in dim_case]
 
-            input = torch.FloatTensor(*test_dims).uniform_(-1, 1).cuda()
-            print("Shape: ", input.shape)
+                    input = torch.FloatTensor(*test_dims).uniform_(-1, 1).cuda()
+                    print("Shape: ", input.shape)
+                    print("Bit error rate: ", p_case)
 
-            print("\nuint8")
-            input = torch.ones_like(input)
-            X_cuda_uint8 = input.type(torch.uint8)
-            X_cuda_uint8 = cuda_profiler(bfitt.bfi_8bit, X_cuda_uint8, p01, p10)
-            #print(X_cuda_uint8)
-            del X_cuda_uint8
+                    print("\nuint8")
+                    input = torch.ones_like(input)
+                    X_cuda_uint8 = input.type(torch.uint8)
+                    X_cuda_uint8 = cuda_profiler(bfitt.bfi_8bit, X_cuda_uint8, p_case[0], p_case[1])
+                    print(X_cuda_uint8)
+                    del X_cuda_uint8
 
-            print("\nint8")
-            input = torch.ones_like(input)
-            X_cuda_int8 = input.type(torch.int8)
-            X_cuda_int8 = cuda_profiler(bfitt.bfi_8bit, X_cuda_int8, p01, p10)
-            #print(X_cuda_uint8)
-            del X_cuda_int8
+                    print("\nint8")
+                    input = torch.ones_like(input)
+                    X_cuda_int8 = input.type(torch.int8)
+                    X_cuda_int8 = cuda_profiler(bfitt.bfi_8bit, X_cuda_int8, p_case[0], p_case[1])
+                    print(X_cuda_int8)
+                    del X_cuda_int8
 
-            print("\nint16")
-            input = torch.ones_like(input)
-            X_cuda_int16 = input.type(torch.int16)
-            X_cuda_int16 = cuda_profiler(bfitt.bfi_16bit, X_cuda_int16, p01, p10)
-            #print(X_cuda_int16)
-            del X_cuda_int16
+                    print("\nint16")
+                    input = torch.ones_like(input)
+                    X_cuda_int16 = input.type(torch.int16)
+                    X_cuda_int16 = cuda_profiler(bfitt.bfi_16bit, X_cuda_int16, p_case[0], p_case[1])
+                    print(X_cuda_int16)
+                    del X_cuda_int16
 
-            print("\nint32")
-            input = torch.ones_like(input)
-            X_cuda_int32 = input.type(torch.int32)
-            X_cuda_int32 = cuda_profiler(bfitt.bfi_32bit, X_cuda_int32, p01, p10)
-            #print(X_cuda_int32)
-            del X_cuda_int32
+                    print("\nint32")
+                    input = torch.ones_like(input)
+                    X_cuda_int32 = input.type(torch.int32)
+                    X_cuda_int32 = cuda_profiler(bfitt.bfi_32bit, X_cuda_int32, p_case[0], p_case[1])
+                    print(X_cuda_int32)
+                    del X_cuda_int32
 
-            print("\nfloat32")
-            input = torch.ones_like(input)
-            X_cuda_float32 = input.type(torch.float32)
-            X_cuda_float32 = cuda_profiler(bfitt.bfi_32bit, X_cuda_float32, p01, p10)
-            #print(X_cuda_int32)
-            del X_cuda_float32
+                    print("\nfloat32")
+                    input = torch.ones_like(input)
+                    X_cuda_float32 = input.type(torch.float32)
+                    X_cuda_float32 = cuda_profiler(bfitt.bfi_32bit, X_cuda_float32, p_case[0], p_case[1])
+                    print(X_cuda_float32)
+                    del X_cuda_float32
 
-            print("\nint64")
-            input = torch.ones_like(input)
-            X_cuda_int64 = input.type(torch.int64)
-            X_cuda_int64 = cuda_profiler(bfitt.bfi_64bit, X_cuda_int64, p01, p10)
-            #print(X_cuda_int64)
-            del X_cuda_int64
+                    print("\nint64")
+                    input = torch.ones_like(input)
+                    X_cuda_int64 = input.type(torch.int64)
+                    X_cuda_int64 = cuda_profiler(bfitt.bfi_64bit, X_cuda_int64, p_case[0], p_case[1])
+                    print(X_cuda_int64)
+                    del X_cuda_int64
 
-            print("\nfloat64")
-            input = torch.ones_like(input)
-            X_cuda_float64 = input.type(torch.float64)
-            X_cuda_float64 = cuda_profiler(bfitt.bfi_64bit, X_cuda_float64, p01, p10)
-            #print(X_cuda_int64)
-            del X_cuda_float64
+                    print("\nfloat64")
+                    input = torch.ones_like(input)
+                    X_cuda_float64 = input.type(torch.float64)
+                    X_cuda_float64 = cuda_profiler(bfitt.bfi_64bit, X_cuda_float64, p_case[0], p_case[1])
+                    print(X_cuda_float64)
+                    del X_cuda_float64
 
-            print("\n\n---\n\n")
-            del input
+                    print("\n\n---\n\n")
+                    del input
 
     for obj in gc.get_objects():
         try:
